721dfd497c45085ae79967240e28bc2f012b1061
Khushal Mali <khushalmali8448@gmail.com>
2024-12-06 10:48:44 +0530

    added get user query and hook in backend

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -4,22 +4,45 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useAuthActions } from "@convex-dev/auth/react";
+import { Loader, LogOut } from "lucide-react";
+import { useCurrentUser } from "../hooks/use-current-user";
 
 const UserButton = () => {
+  const { data, isLoading } = useCurrentUser();
+  const { signOut } = useAuthActions();
+
+  if (isLoading) {
+    return <Loader className="size-4 animate-spin text-muted-foreground" />;
+  }
+
+  if (!data) {
+    return null;
+  }
+
+  const { name, image } = data;
+  const avatarFallback = name?.charAt(0).toUpperCase();
+
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger className="outline-none relative">
         <Avatar className="size-10 hover:opacity-75 transition">
-          <AvatarImage />
-          <AvatarFallback></AvatarFallback>
+          <AvatarImage alt="Name" src={image} />
+          <AvatarFallback className="bg-sky-400 text-white font-semibold">
+            {avatarFallback}
+          </AvatarFallback>
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="center" side="right" className="w-60">
-        <DropdownMenuItem></DropdownMenuItem>
+        <DropdownMenuItem
+          onClick={() => signOut()}
+          className="cursor-pointer h-10"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log Out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
