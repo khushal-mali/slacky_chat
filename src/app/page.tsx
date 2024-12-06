938e16ac6774876620ae4e93ee4f813d98d4d578
Khushal Mali <khushalmali8448@gmail.com>
2024-12-06 11:21:48 +0530

    added workspaces schema and dialog comp.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import UserButton from "@/features/auth/components/user-button";
-// import { useAuthActions } from "@convex-dev/auth/react";
 
 export default function Home() {
-  // const { signOut } = useAuthActions();
-
   return (
     <div className="">
       Logged In
