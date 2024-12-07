6a5c0cfd75095747e90750a258ab2d6d3ca5a24b
Khushal Mali <khushalmali8448@gmail.com>
2024-12-07 19:34:52 +0530

    workspace id api

---
+++ src/features/workspaces/api/use-get-workspace.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "convex/react";
+import { api } from "../../../../convex/_generated/api";
+import { Id } from "../../../../convex/_generated/dataModel";
+
+interface UseGetWorkspaceProps {
+  id: Id<"workspaces">;
+}
+
+const useGetWorkspace = ({ id }: UseGetWorkspaceProps) => {
+  const data = useQuery(api.workspaces.getById, {
+    id,
+  });
+
+  const isLoading = data === undefined;
+
+  return { data, isLoading };
+};
+
+export default useGetWorkspace;
