a660d83be4c1a38491d67635464cef01e65ef328
Khushal Mali <khushalmali8448@gmail.com>
2024-12-09 23:24:45 +0530

    get workspace query updated

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -46,7 +46,17 @@ export const get = query({
       .withIndex("by_user_id", (q) => q.eq("userId", userId))
       .collect();
 
-    return await ctx.db.query("workspaces").collect();
+    const workspaceIds = members.map((member) => member.workspaceId);
+
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) workspaces.push(workspace);
+    }
+
+    return workspaces;
   },
 });
 
