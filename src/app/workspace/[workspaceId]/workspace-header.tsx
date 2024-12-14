8ed54f4e8af179dcdc08cd7246f77a2c0b27bde4
Khushal Mali <khushalmali8448@gmail.com>
2024-12-15 01:16:31 +0530

    modal added to header

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -8,7 +8,9 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ChevronDown, ListFilter, SquarePen } from "lucide-react";
+import { useState } from "react";
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { PreferencesModal } from "./preferences-model";
 
 interface WorkspaceHeaderProps {
   workspace: Doc<"workspaces">;
@@ -16,65 +18,78 @@ interface WorkspaceHeaderProps {
 }
 
 const WorkspaceHeader = ({ workspace, isAdmin }: WorkspaceHeaderProps) => {
+  const [preferencesOpen, setPreferencesOpen] = useState(false);
+
   return (
-    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant="transparent"
-            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
-            size="sm"
-          >
-            <span className="truncate">{workspace?.name}</span>
-            <ChevronDown className="shrink-0 size-4 ml-1" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent side="bottom" align="start" className="w-64">
-          <DropdownMenuItem className="cursor-pointer capitalize">
-            <div className="size-9 relative mr-2 overflow-hidden text-white text-xl rounded-md flex items-center justify-center font-semibold bg-[#616061]">
-              {workspace.name.charAt(0).toUpperCase()}
-            </div>
-            <div className="flex flex-col items-start ">
-              <p className="font-bold">{workspace?.name}</p>
-              <p className="text-xs text-muted-foreground">Active workspace</p>
-            </div>
-          </DropdownMenuItem>
+    <>
+      <PreferencesModal
+        open={preferencesOpen}
+        setOpen={setPreferencesOpen}
+        initialValue={workspace.name}
+      />
 
-          {isAdmin && (
-            <>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="cursor-pointer py-2"
-                onClick={() => {}}
-              >
-                Invite people to {workspace.name}
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem
-                className="cursor-pointer py-2"
-                onClick={() => {}}
-              >
-                Preferences
-              </DropdownMenuItem>
-            </>
-          )}
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="transparent"
+              className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+              size="sm"
+            >
+              <span className="truncate">{workspace?.name}</span>
+              <ChevronDown className="shrink-0 size-4 ml-1" />
+            </Button>
+          </DropdownMenuTrigger>
 
-      <div className="flex items-center gap-0.5">
-        <Hint label="Filter conversations" side="bottom">
-          <Button variant="transparent" size="sm">
-            <ListFilter className="size-4" />
-          </Button>
-        </Hint>
+          <DropdownMenuContent side="bottom" align="start" className="w-64">
+            <DropdownMenuItem className="cursor-pointer capitalize">
+              <div className="size-9 relative mr-2 overflow-hidden text-white text-xl rounded-md flex items-center justify-center font-semibold bg-[#616061]">
+                {workspace.name.charAt(0).toUpperCase()}
+              </div>
+              <div className="flex flex-col items-start ">
+                <p className="font-bold">{workspace?.name}</p>
+                <p className="text-xs text-muted-foreground">
+                  Active workspace
+                </p>
+              </div>
+            </DropdownMenuItem>
 
-        <Hint label="New message" side="bottom">
-          <Button variant="transparent" size="sm">
-            <SquarePen className="size-4" />
-          </Button>
-        </Hint>
+            {isAdmin && (
+              <>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="cursor-pointer py-2"
+                  onClick={() => {}}
+                >
+                  Invite people to {workspace.name}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem
+                  className="cursor-pointer py-2"
+                  onClick={() => setPreferencesOpen(true)}
+                >
+                  Preferences
+                </DropdownMenuItem>
+              </>
+            )}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <div className="flex items-center gap-0.5">
+          <Hint label="Filter conversations" side="bottom">
+            <Button variant="transparent" size="sm">
+              <ListFilter className="size-4" />
+            </Button>
+          </Hint>
+
+          <Hint label="New message" side="bottom">
+            <Button variant="transparent" size="sm">
+              <SquarePen className="size-4" />
+            </Button>
+          </Hint>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
