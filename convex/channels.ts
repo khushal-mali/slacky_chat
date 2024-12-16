61a86de16d51a6e17d16433e1bea61c6cf026039
Khushal Mali <khushalmali8448@gmail.com>
2024-12-16 17:03:02 +0530

    channel creation mutation added

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,6 +1,40 @@
 import { v } from "convex/values";
-import { query } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
+
+export const create = mutation({
+  args: {
+    name: v.string(),
+    workspaceId: v.id("workspaces"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.workspaceId).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    const parsedName = args.name.replace(/\s+/g, "-").toLowerCase();
+
+    const channelId = await ctx.db.insert("channels", {
+      name: parsedName,
+      workspaceId: args.workspaceId,
+    });
+
+    return channelId;
+  },
+});
 
 export const get = query({
   args: {
@@ -13,7 +47,7 @@ export const get = query({
       return [];
     }
 
-    const member = ctx.db
+    const member = await ctx.db
       .query("members")
       .withIndex("by_workspace_id_user_id", (q) =>
         q.eq("workspaceId", args.workspaceId).eq("userId", userId)
@@ -24,7 +58,7 @@ export const get = query({
       return [];
     }
 
-    const channels = ctx.db
+    const channels = await ctx.db
       .query("channels")
       .withIndex("by_workspace_id", (q) =>
         q.eq("workspaceId", args.workspaceId)
