5879636da41992b587e46a89abb843b1715f6ad3
Khushal Mali <khushalmali8448@gmail.com>
2024-12-23 16:08:04 +0530

    toolbar added to add reactions and edit, delete and reply messages in thread

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import { MessageSquareTextIcon, Pencil, Smile, Trash } from "lucide-react";
+import EmojiPopver from "./emoji-popover";
+import Hint from "./hint";
 import { Button } from "./ui/button";
-import { Smile } from "lucide-react";
 
 interface ToolbarProps {
   isAuthor: boolean;
@@ -16,6 +17,7 @@ const Toolbar = ({
   handleDelete,
   handleEdit,
   handleThread,
+  handleReaction,
   isPending,
   hideThreadButton,
   isAuthor,
@@ -23,9 +25,53 @@ const Toolbar = ({
   return (
     <div className="absolute top-0 right-5">
       <div className="group-hover:opacity-100 opacity-0 transition-opacity border bg-white rounded-md shadow-sm">
-        <Button>
-          <Smile className="" />
-        </Button>
+        <EmojiPopver
+          hint="Add reaction"
+          onEmojiSelect={(emoji) => handleReaction(emoji.native)}
+        >
+          <Button variant={"ghost"} size={"sm"} disabled={isPending}>
+            <Smile className="size-4" />
+          </Button>
+        </EmojiPopver>
+
+        {!hideThreadButton && (
+          <Hint label="Reply in thread">
+            <Button
+              variant={"ghost"}
+              size={"sm"}
+              onClick={handleThread}
+              disabled={isPending}
+            >
+              <MessageSquareTextIcon className="size-4" />
+            </Button>
+          </Hint>
+        )}
+
+        {isAuthor && (
+          <>
+            <Hint label="Edit message">
+              <Button
+                variant={"ghost"}
+                onClick={handleEdit}
+                size={"sm"}
+                disabled={isPending}
+              >
+                <Pencil className="size-4" />
+              </Button>
+            </Hint>
+
+            <Hint label="Delete message">
+              <Button
+                variant={"ghost"}
+                onClick={handleDelete}
+                size={"sm"}
+                disabled={isPending}
+              >
+                <Trash className="size-4" />
+              </Button>
+            </Hint>
+          </>
+        )}
       </div>
     </div>
   );
