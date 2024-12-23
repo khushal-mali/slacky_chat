6b21412821a5a19fc61df8232830802eea9f443f
Khushal Mali <khushalmali8448@gmail.com>
2024-12-23 20:10:13 +0530

    thead panel action hook

---
+++ src/hooks/use-panel.ts
@@ -0,0 +1,21 @@
+import { useParentMessageId } from "@/features/messages/store/use-parent-message-id";
+
+const usePanel = () => {
+  const [parentMessageId, setParentMessageId] = useParentMessageId();
+
+  const onOpenMessage = (messageId: string) => {
+    setParentMessageId(messageId);
+  };
+
+  const onClose = () => {
+    setParentMessageId(null);
+  };
+
+  return {
+    parentMessageId,
+    onOpenMessage,
+    onClose,
+  };
+};
+
+export default usePanel;
