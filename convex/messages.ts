2672cc548ab21de8610af3210a212d0f6ec56936
Khushal Mali <khushalmali8448@gmail.com>
2024-12-24 01:28:03 +0530

    get message by id query added

--- convex/messages.ts
+++ convex/messages.ts
@@ -7,9 +7,7 @@ import { mutation, query, QueryCtx } from "./_generated/server";
 const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
   const messages = await ctx.db
     .query("messages")
-    .withIndex("by_parent_message_id", (q) =>
-      q.eq("parentMessageId", messageId)
-    )
+    .withIndex("by_parent_message_id", (q) => q.eq("parentMessageId", messageId))
     .collect();
 
   if (messages.length === 0) {
@@ -55,11 +53,7 @@ const populateMember = (ctx: QueryCtx, memberId: Id<"members">) => {
   return ctx.db.get(memberId);
 };
 
-const getMember = async (
-  ctx: QueryCtx,
-  workspaceId: Id<"workspaces">,
-  userId: Id<"users">
-) => {
+const getMember = async (ctx: QueryCtx, workspaceId: Id<"workspaces">, userId: Id<"users">) => {
   return ctx.db
     .query("members")
     .withIndex("by_workspace_id_user_id", (q) =>
@@ -130,6 +124,79 @@ export const update = mutation({
   },
 });
 
+export const getById = query({
+  args: {
+    id: v.id("messages"),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    const message = await ctx.db.get(args.id);
+
+    if (!message) {
+      return null;
+    }
+
+    const currentMember = await getMember(ctx, message.workspaceId, userId);
+    if (!currentMember) {
+      return null;
+    }
+
+    const member = await populateMember(ctx, message.memberId);
+    if (!member) {
+      return null;
+    }
+
+    const user = await populateUser(ctx, member.userId);
+    if (!user) {
+      return null;
+    }
+
+    const reactions = await populateReactions(ctx, message._id);
+
+    const reactionsWithCounts = reactions.map((reaction) => {
+      return {
+        ...reaction,
+        count: reactions.filter((r) => r.value === reaction.value).length,
+      };
+    });
+
+    const dedupedReactions = reactionsWithCounts.reduce(
+      (acc, reaction) => {
+        const existingReaction = acc.find((r) => r.value === reaction.value);
+
+        if (existingReaction) {
+          existingReaction.memberIds = Array.from(
+            new Set([...existingReaction.memberIds, reaction.memberId])
+          );
+        } else {
+          acc.push({ ...reaction, memberIds: [reaction.memberId] });
+        }
+
+        return acc;
+      },
+      [] as (Doc<"reactions"> & {
+        count: number;
+        memberIds: Id<"members">[];
+      })[]
+    );
+
+    const reactionsWithoutMemberIdProperty = dedupedReactions.map(({ memberId, ...rest }) => rest);
+
+    return {
+      ...message,
+      image: message.image ? await ctx.storage.getUrl(message.image) : undefined,
+      user,
+      member,
+      reactions: reactionsWithoutMemberIdProperty,
+    };
+  },
+});
+
 export const get = query({
   args: {
     channelId: v.optional(v.id("channels")),
@@ -180,23 +247,18 @@ export const get = query({
 
             const reactions = await populateReactions(ctx, message._id);
             const thread = await populateThread(ctx, message._id);
-            const image = message.image
-              ? await ctx.storage.getUrl(message.image)
-              : undefined;
+            const image = message.image ? await ctx.storage.getUrl(message.image) : undefined;
 
             const reactionsWithCounts = reactions.map((reaction) => {
               return {
                 ...reaction,
-                count: reactions.filter((r) => r.value === reaction.value)
-                  .length,
+                count: reactions.filter((r) => r.value === reaction.value).length,
               };
             });
 
             const dedupedReactions = reactionsWithCounts.reduce(
               (acc, reaction) => {
-                const existingReaction = acc.find(
-                  (r) => r.value === reaction.value
-                );
+                const existingReaction = acc.find((r) => r.value === reaction.value);
 
                 if (existingReaction) {
                   existingReaction.memberIds = Array.from(
