dec682e8414b3d844de3a6d238ea5a7441a255d2
Khushal Mali <khushalmali8448@gmail.com>
2024-12-25 02:54:58 +0530

    emoji popover updated

--- src/components/emoji-popover.tsx
+++ src/components/emoji-popover.tsx
@@ -1,35 +1,25 @@
-import Picker from "@emoji-mart/react";
-import data from "@emoji-mart/data";
-
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import EmojiPicker, { type EmojiClickData } from "emoji-picker-react";
 import React, { useState } from "react";
 
 interface EmojiPopoverProps {
   children: React.ReactNode;
   hint?: string;
-  onEmojiSelect: (emoji: any) => void;
+  onEmojiSelect: (value: string) => void;
 }
 
-const EmojiPopver = ({
-  children,
-  onEmojiSelect,
-  hint = "Emoji",
-}: EmojiPopoverProps) => {
+const EmojiPopver = ({ children, onEmojiSelect, hint = "Emoji" }: EmojiPopoverProps) => {
   const [popoverOpen, setPopoverOpen] = useState(false);
   const [tooltipOpen, setTooltipOpen] = useState(false);
 
-  const onSelect = (emoji: any) => {
-    onEmojiSelect(emoji);
+  const onSelect = (value: EmojiClickData) => {
+    onEmojiSelect(value.emoji);
     setPopoverOpen(false);
 
     setTimeout(() => {
@@ -40,11 +30,7 @@ const EmojiPopver = ({
   return (
     <TooltipProvider>
       <Popover open={popoverOpen} onOpenChange={setPopoverOpen}>
-        <Tooltip
-          open={tooltipOpen}
-          onOpenChange={setTooltipOpen}
-          delayDuration={50}
-        >
+        <Tooltip open={tooltipOpen} onOpenChange={setTooltipOpen} delayDuration={50}>
           <PopoverTrigger asChild>
             <TooltipTrigger asChild>{children}</TooltipTrigger>
           </PopoverTrigger>
@@ -54,7 +40,7 @@ const EmojiPopver = ({
         </Tooltip>
 
         <PopoverContent className="p-0 w-full border-none shadow-none">
-          <Picker data={data} onEmojiSelect={onSelect} />
+          <EmojiPicker onEmojiClick={onSelect} />
         </PopoverContent>
       </Popover>
     </TooltipProvider>
