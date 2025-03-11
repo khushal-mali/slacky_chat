f16af36b6cc3b22985ad4b03960c1f0776cf6b9a
Khushal Mali <khushalmali8448@gmail.com>
2025-03-11 23:09:51 +0530

    update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import { ConvexClientProvider } from "@/components/convex-client-provider";
-import { Modals } from "@/components/modals";
-import { NuqsAdapter } from "nuqs/adapters/next/app";
-import { Toaster } from "@/components/ui/sonner";
-import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
 import JotaiProvider from "@/components/jotai-provider";
+import { Modals } from "@/components/modals";
+import { Toaster } from "@/components/ui/sonner";
+import { constructMetadata } from "@/lib/utils";
+import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
+import { Inter } from "next/font/google";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "The Slacky Chat",
-  description:
-    "Mainly used for workplace communication and collaboration on various tasks and projects.",
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,38 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { Metadata } from "next";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function constructMetadata({
+  title = "The Slacky Chat",
+  description = "Mainly used for workplace communication and collaboration on various tasks and projects.",
+  image = "/thumbnail.png",
+  icons = "/favicon.ico",
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: string;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [{ url: image }],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@khushalmali_x",
+    },
+    icons,
+    metadataBase: new URL("https://slacky-chat-ofne.vercel.app/"),
+  };
 }
